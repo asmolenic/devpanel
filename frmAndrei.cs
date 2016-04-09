c11851667fa7d4d7d389e3f8a6d5e4369d6b60c6
asmolenic <asmolenic@gmail.com>
2016-04-09 19:34:10 +0300

    First commit comment

---
+++ frmAndrei.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevPanel
+{
+    public partial class frmAndrei : Form
+    {
+        frmMain mainForm;
+
+        public frmAndrei()
+        {
+            InitializeComponent();
+
+            //mainForm = (frmMain)this.Owner;
+        }
+
+        private void frmAndrei_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mainForm.Show();
+        }
+
+        private void frmAndrei_Load(object sender, EventArgs e)
+        {
+            mainForm = (frmMain)this.Owner;
+
+        }
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevPanel
+{
+    public partial class frmMain : Form
+    {
+        private frmAndrei frmAS;
+
+        //private frmAndrei FrmAS {
+        //    get {
+        //        if (_frmAS == null)
+        //        {
+        //            _frmAS = new frmAndrei();
+        //            _frmAS.Owner = this;
+        //        }
+
+        //        return _frmAS;
+        //    }
+        //}
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAndrei_Click(object sender, EventArgs e)
+        {
+            frmAS = new frmAndrei();
+            frmAS.Owner = this;
+            frmAS.Show();
+            this.Hide();
+        }
+    }
+}
